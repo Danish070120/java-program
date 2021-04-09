fe25107ec89c9f7b34729ccb4d7c232677fcb6c2
danish070120 <danish070120@gmail.com>
2021-04-09 08:46:31 +0530

    create some file

---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test {
+	public static void main(String args[]) {
+	int b=0;
+	int res = a&&b;*/
+	boolean a = true;
+	boolean b = false;
+	boolean res1 = a&&b;
+	boolean res2 = a||b;
+	System.out.println(res1);
+	System.out.println(res2);
