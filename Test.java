dd8399e16b22e2aeda66a482e3c97dfe0424c67f
danish070120 <danish070120@gmail.com>
2021-04-10 10:42:55 +0530

    file added

--- Test.java
+++ Test.java
@@ -1,10 +1,21 @@
 class Test {
 	public static void main(String args[]) {
-	int b=0;
-	int res = a&&b;*/
-	boolean a = true;
-	boolean b = false;
-	boolean res1 = a&&b;
-	boolean res2 = a||b;
-	System.out.println(res1);
-	System.out.println(res2);
+	int abc[]=new int[10];
+	System.out.println("Size of array is "+abc.length);
+	System.out.println(abc[0]);
+	System.out.println(abc[9]);
+	abc[0]=100;
+	abc[1]=200;
+	System.out.println("Value is "+abc[0]);	
+	System.out.println("Value is "+abc[1]);
+	int temp=1000;
+	for(int i=0;i<abc.length;i++) {
+		abc[i]=temp;
+		temp=temp+100;
+	}
+	System.out.println("using loop display");
+	for(int a:abc){
+		System.out.println(a);
+		}
+	}
+}
